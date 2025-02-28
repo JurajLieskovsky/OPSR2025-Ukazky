f2a569afa2e4b8b5a1c57afc74c064aef0a54640
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2025-02-28 10:22:19 +0100

    bi-rotor eom - input matrix
    This reverts commit 026818977284a3f4cbc847df23980998a6b60160.

--- BiRotorEoM/equations_of_motion.py
+++ BiRotorEoM/equations_of_motion.py
@@ -1,5 +1,5 @@
 from sympy import symbols, Function, Matrix, sin, cos, diff, latex, simplify
-from lagrangian2eom import massMatrix, velocityTerms, potentialTerms
+from lagrangian2eom import massMatrix, velocityTerms, potentialTerms, wrenchMatrix
 
 t = symbols("t")
 
@@ -17,6 +17,14 @@ q = Matrix([x, y, theta])
 kin = 1 / 2 * m * (diff(x, t) ** 2 + diff(y, t) ** 2) + 1 / 2 * I * diff(theta, t) ** 2
 pot = m * g * y
 
+# input matrix
+f = Matrix([-sin(theta), cos(theta)])
+
+r_1 = Matrix([x + a * cos(theta), y + a * sin(theta)])
+r_2 = Matrix([x - a * cos(theta), y - a * sin(theta)])
+
+B = Matrix.hstack(wrenchMatrix(r_1, q) * f, wrenchMatrix(r_2, q) * f)
+
 # Quantities
 M = massMatrix(kin, q, t)
 c = velocityTerms(kin, q, t)
@@ -27,4 +35,5 @@ print("T &=", latex(simplify(kin)), "\\\\")
 print("V &=", latex(simplify(pot)))
 print("M &=", latex(simplify(M)), "\\\\")
 print("c &=", latex(simplify(c)), "\\\\")
-print("\\tau_p &=", latex(simplify(tau_p)))
+print("\\tau_p &=", latex(simplify(tau_p)), "\\\\")
+print("B &=", latex(simplify(B)))
