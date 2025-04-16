54d858fd6d125d3f59706a1338319266c0297f59
Juraj Lieskovsky <lieskovsky.juraj@gmail.com>
2025-04-16 12:07:23 +0200

    cvxpy matrix form

--- QP/qp_cvxpy.py
+++ QP/qp_cvxpy.py
@@ -1,17 +1,18 @@
 import cvxpy as cp
+import numpy as np
 
 n = 3
 p = cp.Variable(n)
 
-a = [20, 25, 30]
-b = [5, 4, 3]
-c = [0.02, 0.015, 0.01]
+a = np.array([20, 25, 30])
+b = np.array([5, 4, 3])
+c = np.diag([0.02, 0.015, 0.01])
 
 p_min = [200, 300, 100]
 p_max = [1000, 1500, 800]
 p_sum = 3000
 
-objective = cp.Minimize(cp.sum(cp.multiply(b, p) + cp.multiply(c, cp.square(p))))
+objective = cp.Minimize(cp.quad_form(p, c) + b.T @ p + cp.sum(a)) 
 constraints = [
     p >= p_min,
     p <= p_max,
