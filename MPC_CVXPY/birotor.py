1f621807f4e28d0676d7aac0520e8dfa1afbe4d5
Juraj Lieskovsky <lieskovsky.juraj@gmail.com>
2025-04-23 12:16:23 +0200

    inf LQR konečná podmínka

--- MPC_CVXPY/birotor.py
+++ MPC_CVXPY/birotor.py
@@ -2,6 +2,7 @@ import numpy as np
 import cvxpy as cp
 from scipy.integrate import ode
 import matplotlib.pyplot as plt
+from scipy.linalg import solve_discrete_are
 
 m = 1
 g = 9.81
@@ -27,6 +28,7 @@ def f(t, x, u):
 
 
 # lin. stavovy popis
+x_eq = [1, 1, 0, 0, 0, 0]
 u_eq = 0.5 * m * g * np.ones(2)
 
 A = np.eye(6) + h * np.array(
@@ -52,11 +54,10 @@ B = h * np.array(
 )
 
 # MPC problem
-# Q_N = 1e6 * np.diag([10, 10, 10, 1, 1, 1])
-N = 1000
+N = 20
 Q = np.diag([10, 10, 10, 1, 1, 1])
 R = np.eye(2)
-# u_max = 2
+Q_N = solve_discrete_are(A, B, Q, R)
 
 # MPC CVXPY problem
 x = cp.Variable((6, N + 1))
@@ -66,47 +67,46 @@ x_init = cp.Parameter(6)
 constraints = [
     x[:, 0] == x_init,
     x[:, 1:] == A @ x[:, :-1] + B @ u,
-    u >= -u_eq[:,np.newaxis],
+    u >= -u_eq[:, np.newaxis],
 ]
 
-objective = cp.Minimize(cp.sum_squares(np.sqrt(Q) @ x) + cp.sum_squares(np.sqrt(R) @ u))
+objective = cp.Minimize(
+    cp.sum_squares(np.sqrt(Q) @ x[:, :-1])
+    + cp.sum_squares(np.sqrt(R) @ u)
+    + cp.quad_form(x[:, N], Q_N)
+)
 
 problem = cp.Problem(objective, constraints)
 
 # MPC simulation
-# M = 1000
-x0 = np.zeros(6)
-x_eq = [-2, -2, 0, 0, 0, 0]
+M = 1000
+x0 = [0, 4, 0, 0, 0, 0]
 
-x_init.value = x0 - x_eq
-problem.solve()
+xs = np.zeros((6, M + 1))
+us = np.zeros((2, M))
 
+solver = ode(f).set_integrator(name="dopri5")
+xs[:, 0] = x0
 
-# xs = np.zeros((6, M + 1))
-# us = np.zeros((2, M))
+for k in range(M):
+    # MPC
+    x_init.value = xs[:, k] - x_eq
+    problem.solve()
+    us[:, k] = u_eq + u.value[:, 0]
 
-# solver = ode(f).set_integrator(name="dopri5")
-# xs[:, 0] = x0
-
-# for k in range(M):
-#     # MPC
-#     x_init.value = xs[:, k] - x_eq
-#     problem.solve()
-#     us[:, k] = u_eq + u.value[:, 0]
-
-#     # Simulation
-#     solver.set_initial_value(xs[:, k], 0).set_f_params(us[:, k])
-#     solver.integrate(h)
-#     xs[:, k + 1] = solver.y
+    # Simulation
+    solver.set_initial_value(xs[:, k], 0).set_f_params(us[:, k])
+    solver.integrate(h)
+    xs[:, k + 1] = solver.y
 
 # Visualization
 fig, (ax1, ax2) = plt.subplots(2, 1)
 
 for i in range(3):
-    ax1.plot(x.value[i, :].T, label=f"x{i}")
+    ax1.plot(xs[i, :].T, label=f"x{i}")
 
 for i in range(2):
-    ax2.plot((u.value[i, :] + u_eq[i, np.newaxis]).T, label=f"u{i}")
+    ax2.plot(us[i, :].T, label=f"u{i}")
 
 ax1.legend()
 ax2.legend()
